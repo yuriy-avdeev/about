9742df90082284d6dc61acbcd5288c3fce2e2bba
Yury Avdeev <avdeiev@gmail.com>
2022-03-08 10:11:01 +0300

    fix routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className='app'>
       <Switch>
-        <Route exact path='/'>
+        <Route exact path='/about'>
           <Main />
         </Route>
         <Route path='*'>
