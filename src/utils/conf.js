1ca02e3b7ba003c032f9fb2d635d6670f6777d5e
Yury Avdeev <avdeiev@gmail.com>
2022-05-31 15:20:09 +0300

    readme

--- src/utils/conf.js
+++ src/utils/conf.js
@@ -2,5 +2,5 @@ export const config = {
     mainTitle: 'Educational project of a student of the Faculty of Web Development.',
     aboutMeName: 'Yury Avdeev',
     aboutMeInfo: 'Front-end developer, 43',
-    aboutMeText: 'I live in Moscow, but soon i will live in Bulgaria. I have a higher legal education, but the main segment of my professional career is connected with the construction industry. I have a wife and four beautiful children. I enjoy watching good movies, reading good books and traveling with my family. Recently, I discovered another hobby - web development. And after I completed the course of study, I decided to devote myself completely to my favorite business.'
+    aboutMeText: 'I live in Varna and do what I love - web development. I have law degree, but the main segment of my professional career is connected with the construction industry. I have a wife and four beautiful children. I enjoy watching good movies, reading good books and traveling with my family. Already in adulthood, I discovered another hobby - web development, which has become one of the best aspects of my life.'
 }
